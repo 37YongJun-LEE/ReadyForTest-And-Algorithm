af4a21065acdd0ea37cadf684902c43896443519
37YongJun-LEE <sark2@naver.com>
2023-02-10 15:38:07 +0900

    [PGS] 네트워크 / LV 3 / https://school.programmers.co.kr/learn/courses/30/lessons/43162

--- PGS/네트워크.py
+++ PGS/네트워크.py
@@ -1,3 +1,37 @@
+from collections import deque
+
+def bfs(computers, start, visited):
+    queue = deque([start])
+    visited[start] = 1
+    return_graph = []
+
+    while queue:
+        v = queue.popleft()
+        # print(v, end=' ')
+        return_graph.append(v)
+        for idx, num in enumerate(computers[v]):
+            if num != 0 and visited[idx] != 1:
+                queue.append(idx)
+                visited[idx] = 1
+    return_graph.sort()
+
+    return return_graph
+
+
+def solution(n, computers):
+    answer = 0
+    answer_set = []
+
+    for i in range(n):
+        visited = [0] * n  # 방문 = 1 , 미방분 = 0
+        bfs_return = bfs(computers, i, visited)
+        if bfs_return not in answer_set:
+            answer_set.append(bfs_return)
+
+    return len(answer_set)
+
+
+
 """
 computers = [
     [1, 1, 0],
--- main.py
+++ main.py
@@ -21,27 +21,3 @@ if __name__ == '__main__':
 """ 이곳은 연습장 입니다. """
 #################################################################
 
-
-
-# DFS와 BFS 구현을 정확히 알아야한다.
-
-# 주어진 그래프에서 연결된 노드를 깊이우선탐색으로 방문하는 법
-def dfs(graph, v, visited):     # DFS는 스택을 이용한다.
-    # 현재 노드를 방문처리
-    visited[v] = True
-    print(v, end=" ")
-
-    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
-    for i in graph[v]:
-        if not visited[i]:              # if not 조건문: ==> 구조 설명: 조건문에 해당하지 않으면 실행하는 if문
-            dfs(graph, i, visited)
-
-
-
-
-visited = [False] * 9
-
-
-
-
-
