83a12f40a826ec3bf2cba39285b75041a8a56ba5
37YongJun-LEE <sark2@naver.com>
2023-03-13 17:06:59 +0900

    [PGS] 문자열 내 마음대로 정렬하기 / LV 1 / https://school.programmers.co.kr/learn/courses/30/lessons/12915

--- PGS/문자열내마음대로정렬하기.py
+++ PGS/문자열내마음대로정렬하기.py
@@ -1,6 +1,7 @@
 def solution(strings, n):
     answer = []
     # n 은 string[0]의 인덱스 기준으로 정렬할것
+    # 먼저 사전순 정렬을 해준다음 n 기준으로 다시 정렬
     strings.sort()
     answer = sorted(strings, key = lambda x: x[n])
     print(answer)
