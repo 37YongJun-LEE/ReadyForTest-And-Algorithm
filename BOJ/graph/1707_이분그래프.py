82bad2117243a1adfe3ebf2340e3519b6942e0c0
37YongJun-LEE <sark2@naver.com>
2023-03-15 18:24:42 +0900

    [BOJ] 이분 그래프 / 골드 4 / https://www.acmicpc.net/problem/1707

--- BOJ/graph/1707_이분그래프.py
+++ BOJ/graph/1707_이분그래프.py
@@ -1,53 +1,76 @@
 import sys
-sys.setrecursionlimit(200000)
+sys.setrecursionlimit(10**6)
 
-# DFS 구현하기
-def dfs(start):
-    visited[start] = True
-    #print(start, end=' ')
+# 테스트 케이스의 개수
+t = int(sys.stdin.readline())
 
-    for k in graph[start]:
-        if not visited[k]:
-            return dfs(k)
+# 현재 노드와 이웃한 노드 탐색
+def dfs(node):
 
+    global result
 
-n = int(input())        # 총 그래프 개수 입력
-for i in range(n):
-    V, E = map(int, sys.stdin.readline().split())   # 각 그래프당 추어지는 노드와 간선 개수 입력
-    graph = [ [] for i in range(V+1) ]
-    visited = [False]*(V+1)
-    
-    for j in range(1, E+1):     # 그래프 각 노드당 연결된 정보 입력
-        node1, node2 = map(int, sys.stdin.readline().split())
-        graph[node1].append(node2)
-        graph[node2].append(node1)      # 그래프 그리기 완료
+    for neighbor in graph[node]:
 
-    #확인코드
-    #print(graph)
-    #print(visited)
+        # 이웃 노드에 색칠이 되어있지 않다면
+        if (visited[neighbor] == -1):
 
+            # 현재 노드가 1로 색칠되어있다면
+            if (visited[node] == 1):
+                visited[neighbor] = 2
+            # 현재 노드가 2로 색칠되어있다면
+            if (visited[node] == 2):
+                visited[neighbor] = 1
 
-    # 입력받은 그래프로 YES, NO 판별하기
-    if V > E:
-        dfs(1)
-        visited.pop(0)
-        if False not in visited:
-            print('YES')
+            dfs(neighbor)
+
+        # 이웃 노드에 색칠이 되어있다면
         else:
-            print('NO')
+
+            # 현재 노드와 동일한 색깔이라면
+            if (visited[node] == visited[neighbor]):
+
+                result = False
+                return
+
+for _ in range(t):
+
+    # 정점의 개수 V와 간선의 개수 E
+    v, e = map(int, sys.stdin.readline().split())
+
+    # 색칠 여부 저장
+    visited = [-1] * (v+1)
+
+    # 양방향 간선 정보 저장
+    graph = [[] for _ in range(v+1)]
+
+    # 양방향 간선 정보 저장
+    for _ in range(e):
+        start, end = map(int, sys.stdin.readline().split())
+        graph[start].append(end)
+        graph[end].append(start)
+
+    # 이분 그래프 여부
+    result = True
+
+    # 모든 노드 탐색
+    for i in range(1, v+1):
+
+        # 현재 노드가 색칠 되어있지 않다면
+        if (visited[i] == -1):
+
+            # 현재 노드를 색칠
+            visited[i] = 1
+
+            # 이웃한 노드에 대해서 색칠
+            dfs(i)
+
+            # 현재 노드로부터 이분 그래프가 아닌것을 확인했다면
+            if (result == False):
+                break
+
+    # 이분 그래프가 아니라면
+    if (result == False):
+        print("NO")
+    # 이분 그래프라면
     else:
-        print('NO')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        print("YES")
