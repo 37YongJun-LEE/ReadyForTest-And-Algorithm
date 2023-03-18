5aca0133a95a2f521f97cd4160383fad3b8302e0
37YongJun-LEE <sark2@naver.com>
2023-03-18 12:29:50 +0900

    [PGS] 짝지어 제거하기 / LV 2 / https://school.programmers.co.kr/learn/courses/30/lessons/12973

---
+++ PGS/짝지어제거하기.py
@@ -0,0 +1,22 @@
+def solution(s):
+    answer = -1
+    cnt = 0
+    maximum = len(s)
+
+    while s:
+        s_list = list(s)
+        for i in range(1, len(s_list)):
+            if s_list[i] == s_list[i - 1]:
+                s_list[i] = ''
+                s_list[i - 1] = ''
+                s = "".join(s_list)
+
+        cnt += 1
+        if maximum == cnt:
+            break
+    if not s:
+        return 1
+    else:
+        return 0
+
+    return answer
