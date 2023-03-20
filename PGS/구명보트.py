3c35b2515a1f30b63bca65c45feb6df662793b27
37YongJun-LEE <sark2@naver.com>
2023-03-20 16:25:25 +0900

    [PGS] 구명보트 / LV 2 / https://school.programmers.co.kr/learn/courses/30/lessons/42885

--- PGS/구명보트.py
+++ PGS/구명보트.py
@@ -1,16 +1,15 @@
+from collections import deque
 def solution(people, limit):
     answer = 0
     people.sort(reverse=True)
-    while people:
-        v = people.pop(0)
+    people2 = deque(people)
+    while people2:
+        v = people2.popleft()
         k = limit - v
-        for i in range(len(people)):
-            if people[i] <= k:
-                people.pop(i)
-                break
+        if people2[-1] <= k:
+            people2.pop()
         answer += 1
-        if len(people) == 1:
+        if len(people2) == 1:
             answer += 1
             break
-
     return answer
