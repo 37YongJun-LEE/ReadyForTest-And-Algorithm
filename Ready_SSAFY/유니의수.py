0c7ee7cc92b51b257d4f3b61f9435efe1d51f820
37YongJun-LEE <sark2@naver.com>
2023-05-15 13:49:34 +0900

    [ALL] 유니의 수 / 빈출 유형

--- Ready_SSAFY/유니의수.py
+++ Ready_SSAFY/유니의수.py
@@ -1,3 +1,31 @@
+N = int(input())
+data = [1] * 10 # 2 ~ 9 진법일 경우의 유니수 저장할공간 마련
+# 진법 변환하는 코드
+for i in range(2, 10):
+    now_decimal = i
+    tmp = N
+    answer = []
+    while tmp > 0:
+        answer.append(tmp % now_decimal)
+        tmp = tmp // now_decimal
+
+    for k in answer:
+        if k == 0: continue
+        data[i] *= k
+
+max_decimal = 0
+max_yuni = 0
+for i in range(2, 10):
+    if max_yuni <= data[i]:
+        max_decimal = i
+        max_yuni = data[i]
+print( max_decimal, max_yuni)
+
+
+
+
+
+"""
 num = int(input())
 
 data = [1] * 10
@@ -21,3 +49,4 @@ for i in range(2, 10):
         remain = i
 
 print(remain, yuni_num)
+"""
