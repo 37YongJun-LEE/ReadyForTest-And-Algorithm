edaa0fab67af1e4c7a94a3b654fca112a961b2b1
37YongJun-LEE <sark2@naver.com>
2023-05-16 15:37:26 +0900

    [ALL] 파이널 캠프 C / 파이널 캠프

--- Ready_SSAFY/파이널캠프C.py
+++ Ready_SSAFY/파이널캠프C.py
@@ -126,20 +126,22 @@ def check(x):
     visited = [False for i in range(n)]
     visited[x] = True
     while True:
-        idx = -1
-        d = 10000000
-        for i in range(n):
-            if s[i] == 'o' and not visited[i]:
-                if abs(i - x) < d:
-                    idx = i
-                    d = abs(i - x)
+        idx = -1 # 가장 가까운 친구와의 좌표 초기값
+        d = 10000000 # 그때의 거리 초기값
+        for i in range(n): # 현재 위치 x 로 부터, 모든 'o' 친구위치의 거리를 계산하기 
+            if s[i] == 'o' and not visited[i]: # 해당 위치가 o이고 방문기록이 없다면,
+                # 현재 위치가 x 이고, 친구위치 i 와의 거리를 계산하는 경우
+                if abs(i - x) < d:  # 거리 == abs(i - x) 가 d 보다 작다면, 지금까지 제일 가까운 것보다 가까운 경우를 찾은 것이므로,
+                    idx = i         # 따라서 가장 가까운 좌표 변경
+                    d = abs(i - x)   # 가장 가까운 경우와 거리도 갱신
+                #인데, 만약에 가장 가까운 경우랑, 해당 차이가같은 경우가 존재한다면, 거리가 같은 경우가 2개이므로 False
                 elif abs(i - x) == d:
                     return False
 
-        if idx == -1:
+        if idx == -1: # idx == -1이란 말은 하나도 방문할 경우가 없다는 것, 따라서 모두 다 방문한 경우이므로 while 문 빠져나온다.
             break
-    x = idx
-    visited[x] = True
+    x = idx # 현재위치 갱신하기
+    visited[x] = True # 위치 방문처리하기
 
     return True
 
