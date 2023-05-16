b000d57d3eca8b2755b86e67291d940324d09d22
37YongJun-LEE <sark2@naver.com>
2023-05-16 17:24:02 +0900

    [ALL] practice / 2023.5.16

--- practice.py
+++ practice.py
@@ -1,4 +1,159 @@
-from collections import deque
+"""dx = [-1, +1, 0, 0] #행: 상 하 좌 우
+dy = [0, 0, -1, +1] # 열
+
+def check(x, y, LRUD):
+    nx = x + dx[LRUD]
+    ny = y + dy[LRUD]
+    if nx < 0 or ny < 0 or nx >= N or ny >= N:
+        return False
+
+    if data[x][y] != data[nx][ny]:
+        return True
+    else:
+        return False
+
+def count(x, y):
+    answer = 0
+    # 가로 경우
+    x_cnt = 1
+    prv_x = ''
+    for x1 in data[x]:
+        if x1 != prv_x: # 다른경우
+            x_cnt = 1
+        else: # 같은경우
+            x_cnt += 1
+        prv_x = x1
+        answer = max(x_cnt, answer)
+
+    y_cnt = 1
+    prv_y = ''
+    for i in range(N):
+        if data[i][y] != prv_y:
+            y_cnt = 1
+        else:
+            y_cnt += 1
+        prv_y = data[i][y]
+        answer = max(y_cnt, answer)
+    return answer
+
+# 가장 긴 바둑돌
+T = int(input())
+for test_case in range(1, T+1):
+    N = int(input())
+    data = list( list(input()) for _ in range(N))
+
+    answer = 0
+    for l in range(N):
+        answer = max(answer, count(l, l))
+
+    for i in range(N):
+        for j in range(N):
+            for k in range(4):
+                if check(i, j, k):
+                    # 변경 가능하다면 바꾸기
+                    data[i][j], data[i + dx[k]][j + dy[k]] = data[i + dx[k]][j + dy[k]], data[i][j]
+
+                    answer = max(answer, count(i, j))
+                    answer = max(answer, count(i + dx[k], j + dy[k]))
+
+                    # 원상복귀
+                    data[i][j], data[i + dx[k]][j + dy[k]] = data[i + dx[k]][j + dy[k]], data[i][j]
+
+    print(answer)
+"""
+"""
+5
+5
+BYRBG
+YRYGB
+RBRYY
+BGYBG
+BGYGR
+6
+YRGRRR
+BYGGRR
+RBBGBR
+RBRRGB
+RGBYRY
+GYGYGG
+3
+BRG
+GGG
+BYG
+5
+GGYBY
+BRBBY
+YRBBR
+RBRYG
+GRYYY
+4
+GYBR
+YYGY
+BBYR
+YBYR
+
+"""
+
+
+"""T = int(input())
+for test_case in range(1, T+1):
+    N = int(input())
+
+    triangle = []
+    for i in range(N):
+        triangle.append([1] * (i+1))
+
+    for i in range(N):
+        for j in range(1, i):
+            triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
+
+    print('#{}'.format(test_case))
+    for i in range(N):
+        for k in triangle[i]:
+            print(k, end = ' ')
+        print()
+"""
+"""
+####### 파이널 캠프 C 2번 망설임
+def check(x):
+    visited[x] = True
+
+    while True:
+        idx = -1  ## 가장 가까운 친구와의 좌표
+        d = 5000  ## 가장 가까운 친구와의 거리
+
+        for i in range(N):
+            if data[i] == 'o' and not visited[i]:
+                # 현재위치 x 와 모든 i 사이의 거리를 구해서 가장 거리가 작은 경우에 ok
+                if abs(x-i) < d: # 그치 현재 계산한 거리가 d 보다 작은 경우에 갱신되어야지
+                    d = abs(x-i) # 거리 갱신
+                    idx = i # 좌표 갱신
+                elif d == abs(x-i): # 만약 거리가 같은 경우가 있다면 False
+                    return False
+
+        if idx == -1: # idx가 -1인 경우는 모든 위치를 방문한 경우 따라서 전체 반복문 탈출
+            break
+
+        x = idx
+        visited[x] = True
+
+    return True
+
+T = int(input())
+for test_case in range(1, T+1):
+    N = int(input())
+    data = list(input())
+    cnt = 0
+    for i in range(N):
+        visited = [False] * N
+        if check(i):
+            cnt += 1
+
+    print('#{} {}'.format(test_case, cnt))
+"""
+
+
+"""from collections import deque
 
 T = int(input())
 for test_case in range(1, T+1):
@@ -18,7 +173,7 @@ for test_case in range(1, T+1):
             break
     print()
 
-
+"""
 
 """
 2
