022c2c40ba884d9f3ea1802d22816641871aae87
37YongJun-LEE <sark2@naver.com>
2023-05-22 19:11:23 +0900

    [PGS] 타겟 넘버 / LV 2 / https://school.programmers.co.kr/learn/courses/30/lessons/43165

--- PGS/타겟넘버.py
+++ PGS/타겟넘버.py
@@ -1,3 +1,4 @@
+# 다른 사람 풀이
 def dfs(numbers, target, idx, sum_n):
     if len(numbers) == idx:
         return 1 if sum_n == target else 0
@@ -8,3 +9,21 @@ def solution(numbers, target):
     answer = 0
     answer = dfs(numbers, target, 0, 0)
     return answer
+
+"""
+### 내 풀이
+
+answer = 0
+def dfs(numbers, target, idx, total):
+    global answer
+    if idx == len(numbers):
+        if total == target:
+            answer += 1
+        return
+    dfs(numbers, target, idx + 1, total + numbers[idx])
+    dfs(numbers, target, idx + 1, total - numbers[idx])
+    
+def solution(numbers, target):
+    dfs(numbers, target,  0, 0)    
+    return answer
+"""
