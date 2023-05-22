497ea99f7dcd8183f3bcacd6c03c9e337cf2605f
37YongJun-LEE <sark2@naver.com>
2023-05-22 20:02:03 +0900

    [PGS] 주식 가격 / LV 2 / https://school.programmers.co.kr/learn/courses/30/parts/12081

---
+++ PGS/주식가격.py
@@ -0,0 +1,17 @@
+from collections import deque
+def solution(prices):
+    answer = []
+    queue = deque(prices)
+
+    while queue:
+        q = queue.popleft()
+        seq = 0
+
+        for i in queue:
+            if q <= i:
+                seq += 1
+            else:
+                seq += 1
+                break
+        answer.append(seq)
+    return answer
